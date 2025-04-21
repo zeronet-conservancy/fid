61266af6eb9b7839f33e719a8ad580d1fef5ffab
caryoscelus <caryoscelus@gmx.com>
2025-04-21 15:10:56 +0000

    Add stats

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
   import { onMount } from 'svelte';
 
-  let { znAPI, baseAddr } = $props();
+  let { data, baseAddr } = $props();
+  let { znAPI } = data;
 
   let connections = $state([]);
+
+  const serverInfoPromise = znAPI.getServerInfo()
 </script>
 
 <h1>Stats</h1>
 <h2>Client info</h2>
-<p>version:</p>
+<div>
+  {#await serverInfoPromise}
+    ??
+  {:then serverInfo}
+    <p>version: {serverInfo.version} {serverInfo.platform}/{serverInfo.dist_type}</p>
+    <p>listening on: {serverInfo.ui_ip}:{serverInfo.ui_port}</p>
+    <p>tor status: {serverInfo.tor_status}</p>
+  {:catch err}
+    Error while fetching info: {err}
+  {/await}
+</div>
 <h2>Connections</h2>
 <table>
   <tbody>

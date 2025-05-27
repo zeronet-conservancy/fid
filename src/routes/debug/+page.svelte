1d61f8aab5c72a7f6719dc3751f16fc7924cc62e
caryoscelus <caryoscelus@gmx.com>
2025-05-27 11:02:52 +0000

    Debug page

--- src/routes/debug/+page.svelte
+++ src/routes/debug/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
-  
+  import { onMount } from 'svelte';
+
+  let { data } = $props();
+  let { znAPI } = data;
+
+  let messages = $state([]);
+
+  onMount(() => {
+    znAPI.msgSubscribe(
+      (message) => {
+        messages.push(message);
+      }
+    );
+  });
 </script>
 
 <h1>Debug</h1>
+
+<div>
+  {#each messages.slice().reverse() as message}
+    <div>
+      {JSON.stringify(message)}
+    </div>
+  {/each}
+</div>
 
 <style></style>
